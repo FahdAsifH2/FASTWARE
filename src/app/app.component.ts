16f457db05217f1f90d0f777fffac9fbf9661568
Fahd Asif <151466690+FahdAsifH2@users.noreply.github.com>
2024-08-01 12:47:24 -0700

    Updated Navbar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./components/home/home.component";
+import { HomeComponent } from './components/home/home.component';
 import { DatabaseComponent } from './components/database/database.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet,HomeComponent,DatabaseComponent,NavbarComponent,FooterComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+    DatabaseComponent,
+    NavbarComponent,
+    FooterComponent,
+    MatToolbarModule,
+    MatIconModule // Import MatIconModule here
+  ]
 })
-export class AppComponent
-{
-  title = 'fastware';
+export class AppComponent {
+  title = 'FASTWARE';
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { AboutComponent } from '../about/about.component';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [HomeComponent,AboutComponent],
+  imports: [HomeComponent,AboutComponent,MatToolbarModule,MatIconModule,MatButtonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
